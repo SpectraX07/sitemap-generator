1d09f6d044948da2526679f0f0a25240782232c9
Subrata <webwanderer007@gmail.com>
2024-12-14 12:55:20 +0530

    removed htmla and integrated ejs

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // app.js
 import express from 'express';
-import dotenv from 'dotenv';
+import dotenv from 'dotenv/config';
 import manageRoutes from './routes/manageRoutes.js';
 import webRoutes from './routes/webRoutes.js';
 import apiRoutes from './routes/apiRoutes.js';
-
-dotenv.config();
+import bodyParser from 'body-parser';
+import path from 'path';
 
 const app = express();
+const __dirname = path.resolve(); // If using ES modules
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 
 // Use the sitemap routes
 // app.use('/', manageRoutes);
